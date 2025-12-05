e998f1ecf2816f6066242737c0b2bd2235c55fd1
evgenijbozko <Bozhkoevgen1991@mail.ru>
2025-12-05 21:56:17 +0300

    DayWeek

--- src/DayWeek.java
+++ src/DayWeek.java
@@ -50,7 +50,7 @@ public class DayWeek {
             for (int i = 0; i <= dayEnter; i++) {
                 System.out.println(Work[i]);
             }
-        } else if (dayEnter > 5 || dayEnter < 7) {
+        } else if (dayEnter > 5 || dayEnter < 6) {
             System.out.println("Будний день - " + dayWeek[dayEnter]);
             System.out.println("Список дел: ");
             for (int i = 0; i <= dayEnter; i++) {
